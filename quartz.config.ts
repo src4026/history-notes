ea69c7d1f33a512b47cab7d0fe03aa16e5d334a7
s20 <src4026@skiff.com>
2024-02-13 23:14:55 +0530

    Quartz sync: Feb 13, 2024, 11:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "History Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "src4026.github.io/history-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
